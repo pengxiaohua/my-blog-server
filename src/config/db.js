2f5d8c9457615ca7a20d86b8d8aa00e2f6b09bb6
xiaohua <609370075@qq.com>
2022-07-15 23:57:16 +0800

    feat: add redis configuration

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,10 @@
 // 环境变量
 const env = process.env.NODE_ENV
 
-// 创建连接对象
+// mysql 配置
 let MYSQL_CONF
+// redis 配置
+let REDIS_CONF
 
 if (env === 'dev') {
     MYSQL_CONF = {
@@ -12,6 +14,11 @@ if (env === 'dev') {
         port: '3306',
         database: 'my_blog'
     }
+
+    REDIS_CONF = {
+        port: 6379,
+        host: '127.0.0.1'
+    }
 } else if (env === 'pro') {
     MYSQL_CONF = {
         host: 'localhost',
@@ -20,8 +27,14 @@ if (env === 'dev') {
         port: '3306',
         database: 'my_blog'
     }
+
+    REDIS_CONF = {
+        port: 6379,
+        host: '127.0.0.1'
+    }
 }
 
 module.exports = {
-    MYSQL_CONF
+    MYSQL_CONF,
+    REDIS_CONF
 }
---
+++ src/db/redis.js
@@ -0,0 +1,47 @@
+const redis = require('redis')
+const { REDIS_CONF } = require('../config/db')
+
+// 创建客户端
+const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host)
+
+redisClient.on('error', err => {
+    console.log({err});
+})
+
+function set (key, value) {
+    if (typeof val === 'object') {
+        value = JSON.stringify(val)
+    }
+    redisClient.set(key, value, redis.print)
+}
+
+function get (key) {
+    const promise = new Promise((resolve, reject) => {
+        redisClient.get(key, (err, val) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            if (val === null) {
+                resolve(null)
+                return
+            }
+            try {
+                // 如果是 JSON 格式，需要转换处理，如果不是，则报错，进入catch 中逻辑
+                resolve(
+                    JSON.parse(val)
+                )
+            } catch (ex) {
+                resolve(val)
+            }
+            
+        })
+    })
+
+    return promise
+}
+
+module.exports = {
+    set,
+    get
+}
