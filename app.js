3494e47e523af342830e02c42bf80eb5be34cd1c
xiaohua <609370075@qq.com>
2022-07-18 23:41:10 +0800

    feat: delete console

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const getCookieExpires = () => {
     const d = new Date()
     // 7天后过期
     d.setTime(d.getTime() + ( 7 * 24 * 60 * 60 * 1000))
-    console.log('d.toGMTString(): ', d.toGMTString());
     return d.toGMTString()
 }
 
@@ -88,7 +87,6 @@ const serverHandle = (req, res) => {
             // 设置session
             req.session = sessionData
         }
-        console.log('req.session: ', req.session);
 
         // 处理 Request data
         return getRequestData(req)
