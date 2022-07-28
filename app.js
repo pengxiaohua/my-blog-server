e4b618378487468829dacc68f51713dcaa137a5d
xiaohua <609370075@qq.com>
2022-07-29 00:22:13 +0800

    feat: add logger middleware morgan to log

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const session = require('koa-generic-session')
 const redisStore = require('koa-redis')
+const morgan = require('koa-morgan')
+const path = require('path')
+const fs = require('fs')
 
 const user = require('./routes/user')
 const blog = require('./routes/blog')
@@ -34,6 +37,22 @@ app.use(async (ctx, next) => {
   const ms = new Date() - start
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
+
+// logger middleware: morgan
+const ENV = process.env.NODE_ENV
+if (ENV !== 'production') {
+  // 非线上环境
+  app.use(morgan('dev'))
+} else {
+  // 线上环境
+  const logFileName = path.join(__dirname, 'logs', 'access.log')
+  const writeStream = fs.createWriteStream(logFileName, {
+    flags: 'a'
+  })
+  app.use(morgan('combined', {
+    stream: writeStream
+  }));
+}
 
 // session 配置, 加密串
 app.keys = ['dfSaJ#fd3f4_']
